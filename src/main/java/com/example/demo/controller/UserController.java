fcbf7cbfadec36eba7810fac0ada2f58ae0c5ed9
m-inoue <inouem@analix.co.jp>
2024-07-16 10:57:57 +0900

    add: 井上

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+	 @RequestMapping("user/management")
+	 public String user() {
+		 return "user/management";
+	 }
+	 
+	 
+	
 
 }
---
+++ src/main/java/com/example/demo/entity/UserMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+public class UserMapper {
+	private Integer userId;
+	
+	private String password;
+	
+	private String userName;
+	
+	private 
+
+}
