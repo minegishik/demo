da79638f837fd4d0587e8161344b1b0dec3236ed
minegishik <minegishik@analix.co.jp>
2024-07-17 10:10:52 +0900

    add: 峰岸

--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -1,5 +1,33 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.LoginUserDto;
+import com.example.demo.mapper.LoginMapper;
+
+@Service
 public class LoginService {
+	
+	@Autowired
+	private LoginMapper loginMapper;
+	
+	
+	/**
+	 * ログイン処理
+	 * 
+	 * @param userId
+	 * @param password
+	 * @return String
+	 */
+	public List<LoginUserDto> getLoginUser(Integer userId, String password) {
+		
+		List<LoginUserDto> LoginUserDtoList = loginMapper.getLoginUser(userId, password);
+		
+		System.out.println(LoginUserDtoList);
+		return LoginUserDtoList;
+	}
 
 }
