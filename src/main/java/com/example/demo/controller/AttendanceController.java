775b622f4cc45d5440e50c9267138b69a2c76a2a
minegishik <minegishik@analix.co.jp>
2024-07-17 17:57:52 +0900

    add: 峰岸

--- src/main/java/com/example/demo/controller/AttendanceController.java
+++ src/main/java/com/example/demo/controller/AttendanceController.java
@@ -1,8 +1,22 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/attendance")
 public class AttendanceController {
+	
+	/**
+	 * 勤怠登録画面 初期表示
+	 * 
+	 * 
+	 * @return　勤怠登録画面
+	 */
+	@RequestMapping("/regist")
+	public String regist() {
+		
+		return "attendance/regist";
+	}
 
 }
