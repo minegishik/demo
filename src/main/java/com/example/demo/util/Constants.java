4b97aab33acc7ea298ae096f388469c2814662bd
minegishik <minegishik@analix.co.jp>
2024-07-22 14:40:50 +0900

    add:

--- src/main/java/com/example/demo/util/Constants.java
+++ src/main/java/com/example/demo/util/Constants.java
@@ -2,14 +2,14 @@ package com.example.demo.util;
 
 
 
-public class Constants {
+public interface Constants {
 	
 	/**ロール:システム管理者 */
 	public static final String CODE_VAL_ROLL_ADMIN = "0001";
 	/**ロール:ユニットマネージャー */
 	public static final String CODE_VAL_ROLL_UNITMANAGER = "0002";
 	/**ロール:マネージャー */
-	public static final String CODE_VAL_ROLL_MANAGER = "0003";
+	public static final String CODE_VAL_ROLL_MANAGER  = "0003";
 	/**ロール:社員 */
 	public static final String CODE_VAL_ROLL_REGULAR = "0004";
 
