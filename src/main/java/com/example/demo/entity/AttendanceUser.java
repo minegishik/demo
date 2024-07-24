f756d5d4b56b5774063073fc320631d9680761f9
minegishik <minegishik@analix.co.jp>
2024-07-24 17:39:30 +0900

    add: 峰岸

--- src/main/java/com/example/demo/entity/AttendanceUser.java
+++ src/main/java/com/example/demo/entity/AttendanceUser.java
@@ -3,21 +3,28 @@ package com.example.demo.entity;
 import java.sql.Time;
 import java.util.Date;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
+@Component
 @Data
 public class AttendanceUser {
 	
 	//ユーザーID
-		private Integer userId;
-		//勤務状況
-		private Short status;
-		//日付
-		private Date date;
-		//出勤時間
-		private Time startTime;
-		//退勤時間
-		private Time endTime;
+	private Integer userId;
+	//勤務状況
+	private Short status;
+	//日付
+	private Date date;
+	//出勤時間
+	private Time startTime;
+	//退勤時間
+	private Time endTime;
+	//備考
+	private String remarks;
+	//勤怠ID
+	private Integer attendanceId;
 		
 
 }
