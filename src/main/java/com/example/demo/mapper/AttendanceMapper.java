bfccba2c490e14d83cc350836e33da3497ac8c3c
minegishik <minegishik@analix.co.jp>
2024-07-25 17:46:21 +0900

    add:

--- src/main/java/com/example/demo/mapper/AttendanceMapper.java
+++ src/main/java/com/example/demo/mapper/AttendanceMapper.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import com.example.demo.dto.AttendanceDto;
 import com.example.demo.entity.AttendanceUser;
 
 @Mapper
@@ -44,6 +43,6 @@ public interface AttendanceMapper {
 	 * 
 	 * @return 
 	 */
-	List<AttendanceDto>findByAttendanceYearMonth(@Param("userId") int userId, @Param("year") int year, @Param("month") int month);
+	List<AttendanceUser>findByAttendanceYearMonth(@Param("userId") int userId, @Param("year") int year, @Param("month") int month);
 
 }
