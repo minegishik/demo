d97a4ea9bcffa2b24e810cf4dc321d6d2d9a4e24
minegishik <minegishik@analix.co.jp>
2024-07-26 09:26:50 +0900

    add:

---
+++ src/main/java/com/example/demo/form/AttendanceFormList.java
@@ -0,0 +1,12 @@
+package com.example.demo.form;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class AttendanceFormList {
+	
+	private List<AttendanceForm> attendanceFormList;
+
+}
