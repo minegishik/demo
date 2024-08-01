a4d11aa5f678ed2d4a4b59ceeacbde8e0dce60d7
m-inoue <inouem@analix.co.jp>
2024-08-01 17:17:49 +0900

    add: 井上　承認申請機能

--- src/main/java/com/example/demo/service/AttendanceService.java
+++ src/main/java/com/example/demo/service/AttendanceService.java
@@ -10,7 +10,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.MonthlyAttendanceDto;
 import com.example.demo.entity.AttendanceUser;
+import com.example.demo.entity.MonthlyAttendance;
 import com.example.demo.mapper.AttendanceMapper;
 
 @Service
@@ -93,5 +95,83 @@ public class AttendanceService {
     	attendanceMapper.deleteAttendance(userId, date);
     }
 	
+    
+    /**
+	 * 申請該当月の月次勤怠情報取得・申請処理
+	 * 
+	 * @param userId
+	 * @param targetYearMonth
+	 * @param date
+	 * @return
+	 */
+	public String getMonthlyAttendance(Integer userId, Integer year, Integer month) {
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+		LocalDate targetYearMonth = yearMonth.atDay(1);
+		
+		System.out.println(year);
+		System.out.println(month);
+
+		MonthlyAttendance searchResult = attendanceMapper.findMonthlyAttendanceByUserAndMonth(userId,
+				java.sql.Date.valueOf(targetYearMonth));
+
+		MonthlyAttendance request = new MonthlyAttendance();
+		request.setUserId(userId);
+		request.setTargetYearMonth(java.sql.Date.valueOf(targetYearMonth));
+		request.setDate(java.sql.Date.valueOf(LocalDate.now()));
+		request.setStatus((short) 1);
+
+		if (searchResult == null) {
+			attendanceMapper.appricationMonthlyAttendance(request);
+			return "承認申請が完了しました。";
+
+		} else {
+
+			request.setId(searchResult.getId());
+			attendanceMapper.update(request);
+			
+
+			System.out.println(searchResult);
+			return "承認申請を更新しました。";
+		}
+
+	}
+
+	/**
+	 * ステータス表示処理
+	 * 
+	 * @param userId
+	 * @param year
+	 * @param month
+	 * @return
+	 */
+	public String checkStatus(Integer userId, Integer year, Integer month) {
+		YearMonth yearMonth = YearMonth.of(year, month);
+		LocalDate targetYearMonth = yearMonth.atDay(1);
+
+		List<MonthlyAttendanceDto> searchResult = attendanceMapper.findMonthlyAttendanceByUserAndMonth2(userId,
+				java.sql.Date.valueOf(targetYearMonth));
+
+		if (searchResult == null || searchResult.isEmpty()) {
+			return "未申請";
+		} 
+		MonthlyAttendanceDto dto = searchResult.get(0);
+		Short status = dto.getStatus();
+		System.out.println(dto.getStatus());
+		
+		// ステータスに応じて結果を返す
+		switch (status) {
+		case 1:
+			return "申請中";
+		case 2:
+			return "承認済";
+		case 3:
+			return "却下";
+		default:
+			return "未申請";
+		}
+		
+
+	}
 	
 }
