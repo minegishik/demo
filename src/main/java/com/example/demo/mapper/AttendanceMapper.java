a230756a6013284e158e6a33d96b7962486a0721
minegishik <minegishik@analix.co.jp>
2024-08-05 16:35:23 +0900

    add:

--- src/main/java/com/example/demo/mapper/AttendanceMapper.java
+++ src/main/java/com/example/demo/mapper/AttendanceMapper.java
@@ -105,6 +105,6 @@ public interface AttendanceMapper {
 	 * @param userId
 	 * @return 月次勤怠情報DTO
 	 */
-	List<MonthlyAttendanceDto> monthlyAttendanceReqList(@Param("year") int year, @Param("month") int month);
+	List<MonthlyAttendanceDto> monthlyAttendanceReqList();
 	
 }
